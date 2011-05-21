00c8cb700b4c3893ecf5e1d63b7e2a95c75435a2
KatherinaNiv <KatherinaNiv@88d92e4c-988a-edfc-8af1-45151e6552d0>
2011-05-21 18:43:17 +0000

    Created classes

---
+++ task2resource/src/java/logic/Dates.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Катюша
+ */
+public class Dates {
+    protected Date startDate;
+    protected Date finishDate;
+    
+    public Dates() {}
+    
+    public Dates(Date startDate, Date finishDate) {
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+    }
+}
---
+++ task2resource/src/java/logic/Group.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Катюша
+ */
+public class Group {
+    protected String name;
+    protected int oid;
+    protected Set<User> users;
+    
+    public Group() {}
+}
