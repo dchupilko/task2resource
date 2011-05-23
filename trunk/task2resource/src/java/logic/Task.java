4affe56c09e2c67895d9d46963e3474bf28c04fd
petrov.igor.od@gmail.com <petrov.igor.od@gmail.com@88d92e4c-988a-edfc-8af1-45151e6552d0>
2011-05-23 19:49:59 +0000

    Reworked class Task.java

--- trunk/task2resource/src/java/logic/Task.java
+++ trunk/task2resource/src/java/logic/Task.java
@@ -4,8 +4,9 @@
  */
 package logic;
 
-import java.util.Date;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -15,17 +16,85 @@ public class Task {
     protected String name;
     protected int capacity;
     protected int oid;
-    protected HashSet<Dates> dates;
-    protected HashSet<Resource> resources;
+    protected ArrayList<Dates> dates;
+    protected ArrayList<Resource> resources;
+    
+    /*
+     * тут будем хранить коллекцию начала и конца события включая
+     * дату и время. Коллекции синхронны
+     */
+    private ArrayList<GregorianCalendar> eventStarts = null;
+    private ArrayList<GregorianCalendar> eventEnds = null;
+    
+    /*
+     * Тут будем хранить диапазон периодичности события (только дата)
+     * Если событие единоразовое, то поля совпадают
+     */
+    private GregorianCalendar fromDate = null;
+    private GregorianCalendar toDate = null;
+    
+    private int lengthInMinutes = 0;
+    private int[][] period = null;//Переменная задает время и день события
     
     public Task() {}
     
-    public Task(String name, int capacity, Date fromDate, Date toDate, int length) {
-        // TODO: add period
+    /**
+     * 
+     * @param name
+     * @param capacity
+     * Любое событие обязано иметь:
+     * @param fromDate - дату начала
+     * @param toDate - дату конца (если событие повторяется)
+     * @param length - длительность в минутах
+     * @param period - периодичность в виде день-часы-минуты
+     * Иными словами, иметь такой вид
+     *   {1,11,00}//Воскресенье, 11-00
+     *   {2,16,15}//Понедельник, 16-15
+     *   {3,13,00}//Вторник, 13-00
+     * Для единичного события массив будет иметь одну строку.
+     */
+    public Task(String name, int capacity, GregorianCalendar fromDate, GregorianCalendar toDate, int length, int[][]period) {
+        
         this.name = name;
         this.capacity = capacity;
         
-        // TODO: calculateDates(startDate, finishDate, length)
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.lengthInMinutes = length;
+        this.period = period;
+        
+        
+        calculateDates(this.fromDate, this.toDate, this.lengthInMinutes, this.period);
+        
+    }
+    
+    /*
+     * Программа сделает хотя бы один проход (если событие единоразовое)
+     */
+    private void calculateDates(GregorianCalendar fromDate, GregorianCalendar toDate, int length, int[][] period){
+        GregorianCalendar tmpDate               = fromDate;
+        GregorianCalendar templateDateForStart  = null;
+        GregorianCalendar templateDateForFinish = null;
+        do {
+            for (int i = 0; i < period.length; i++) {
+                if (tmpDate.get(Calendar.DAY_OF_WEEK) == period[i][0]) {
+                    templateDateForStart = new GregorianCalendar(tmpDate.get(Calendar.YEAR), 
+                                                                 tmpDate.get(Calendar.MONTH), 
+                                                                 tmpDate.get(Calendar.DAY_OF_MONTH), 
+                                                                 period[i][1], 
+                                                                 period[i][2]);
+                    eventStarts.add(templateDateForStart);
+                    
+                    templateDateForFinish = new GregorianCalendar(tmpDate.get(Calendar.YEAR), 
+                                                                  tmpDate.get(Calendar.MONTH), 
+                                                                  tmpDate.get(Calendar.DAY_OF_MONTH), 
+                                                                  period[i][1] + length/60, 
+                                                                  period[i][1] + length%60);
+                    eventEnds.add(templateDateForFinish); 
+                }
+            }
+            tmpDate.add(Calendar.DATE, 1);
+        } while (!tmpDate.equals(toDate));
     }
     
     public String getName () {
