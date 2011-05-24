67c0cfb34454ed6eb82cb2d9e58f6655774f44b3
petrov.igor.od <petrov.igor.od@gmail.com>
2011-05-24 21:29:41 +0000

    Added mail.jar; log4j.jar. Restored email notificator.

---
+++ task2resource-helios/src/testpackage/main.java
@@ -0,0 +1,25 @@
+package testpackage;
+
+import EmailNotificator.SendNotification;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Testing email sender
+		 
+		System.out.println("Hello world!");
+		String subj = "hello";
+        String message = "hi!";
+        String mail1 = "petrov.igor.od@gmail.com";
+        String[] mail2 = {"petrov.igor.od@gmail.com", "081krieger@gmail.com", "081krieger@xakep.ru"};
+        
+        SendNotification send1 = new SendNotification(mail1, subj + "1", message + "1");
+        SendNotification send2 = new SendNotification(mail2, subj + "2", message + "2");
+	    */
+	}
+
+}
