e79b2ea21a8e60708f19c3de14986421004f5135
petrov.igor.od@gmail.com <petrov.igor.od@gmail.com@88d92e4c-988a-edfc-8af1-45151e6552d0>
2011-05-28 13:32:13 +0000

    RequestMapper_ver.0

--- trunk/task2resource-helios/src/ORM/RequestMapper.java
+++ trunk/task2resource-helios/src/ORM/RequestMapper.java
@@ -1,5 +1,32 @@
 package ORM;
 
-public class RequestMapper {
+import java.util.HashSet;
+import java.util.Set;
+
+import logic.Request;
+
+import org.hibernate.HibernateException;
+
+public class RequestMapper extends AbstractMapper{
 	
+	public Set<Request> getAllRequests(){
+		try{
+			/*
+			 * Supposed "select * from requests;"
+			 */
+			String query = "from Requests";
+			Set<Request> requests = new HashSet(this.readObject(query)); 
+			return requests;
+		} catch(HibernateException he){
+			throw he;
+		}
+	}
+	
+	public void setRequest(Request request){
+		try{
+			this.insertObject(request);
+		}catch(HibernateException he){
+			throw he;
+		}
+	}
 }
