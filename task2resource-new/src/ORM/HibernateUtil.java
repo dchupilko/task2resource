29250936612ee1d74827a7f1ecdd7b4ab9f9c7c6
godor2008 <godor2008@gmail.com>
2011-06-05 19:11:38 +0000

    Исправлены проблмы с подключением к базе. Успешно проходит первый тест.

--- task2resource-new/src/ORM/HibernateUtil.java
+++ task2resource-new/src/ORM/HibernateUtil.java
@@ -8,6 +8,7 @@ public class HibernateUtil {
     static {
       try {
         sessionFactory = new Configuration().configure().buildSessionFactory();
+        
       } catch (Throwable ex) {
         System.err.println("Initial SessionFactory creation failed." + ex);
         throw new ExceptionInInitializerError(ex);
